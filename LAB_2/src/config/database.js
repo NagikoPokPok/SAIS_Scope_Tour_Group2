b9ad7bb1a17f13dc38b147fab482e37c6be190d0
NagikoPokPok <buithanhnghiadn2004@gmail.com>
2025-07-03 08:52:18 +0700

    Revert "Cache part 1"
    This reverts commit bd63dd74e24ba0a81fcf1eb64ab20d762c2b8cf7.

--- LAB_2/src/config/database.js
+++ LAB_2/src/config/database.js
@@ -7,9 +7,6 @@ console.log('DB_NAME:', process.env.DB_NAME);
 console.log('DB_USER:', process.env.DB_USER);
 console.log('DB_HOST:', process.env.DB_HOST);
 console.log('DB_PORT:', process.env.DB_PORT);
-=======
-require('dotenv').config();
->>>>>>> parent of bd63dd7 (Cache part 1)
 
 const sequelize = new Sequelize(
   process.env.DB_NAME,
